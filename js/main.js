35da7450c5fe32db0e112fe98ffb8119bf8e2c8e
Alabi Olasunkanmi <alabiolasunkanmi@outlook.com>
2018-10-02 09:37:47 +0100

    refactor: Refactor JS code to follow MVC design pattern

--- js/main.js
+++ js/main.js
@@ -23,52 +23,142 @@
 
 // });
 
-let catNamesWrapper = document.querySelector('.cat-names');
-let properties = [{
-    name: 'Tony',
-    images: 'images/tony.jpeg',
-    count: 0
-}, {
-    name: 'Kitty',
-    images: 'images/kitty.jpg',
-    count: 0
-}, {
-    name: 'James',
-    images: 'images/james.jpg',
-    count: 0
-}, {
-    name: 'Nelly',
-    images: 'images/nelly.jpg',
-    count: 0
-}, {
-    name: 'Lilly',
-    images: 'images/lilly.jpg',
-    count: 0
-}];
 
-properties.forEach((property) => {
-    let para = document.createElement('p');
-    para.innerHTML = property.name;
-    catNamesWrapper.appendChild(para);
+var model = {
+    currentCat: null,
+    properties: [{
+        name: 'Tony',
+        images: 'images/tony.jpeg',
+        count: 0
+    }, {
+        name: 'Kitty',
+        images: 'images/kitty.jpg',
+        count: 0
+    }, {
+        name: 'James',
+        images: 'images/james.jpg',
+        count: 0
+    }, {
+        name: 'Nelly',
+        images: 'images/nelly.jpg',
+        count: 0
+    }, {
+        name: 'Lilly',
+        images: 'images/lilly.jpg',
+        count: 0
+    }]
+};
 
-    para.addEventListener('click', ((propertyCopy) => {
-        return () => {
-            let catName = document.querySelector('.individual-cat-name');
-            let catImage = document.querySelector('.individual-cat-images');
-            let catClickCount = document.querySelector('.individual-cat-count');
+var octopus = {
+    init: function () {
+        // sets current cat to the first one on the list
+        model.currentCat = model.properties[0];
 
-            catName.textContent = propertyCopy.name;
-            catImage.src = propertyCopy.images;
-            catClickCount.textContent = propertyCopy.count;
+        // tells the views to initialize
+        catListView.init();
+        catView.init();
+    },
+    getCurrentCat: function () {
+        return model.currentCat;
+    },
+    getCats: function () {
+        return model.properties;
+    },
+    // set the currently selected cat to the object passedin
+    setCurrentCat: function (property) {
+        model.currentCat = property;
+    },
 
-            catImage.addEventListener('click', () => {
+    // increments the counter for the currently selected cat
+    incrementCounter: function () {
+        model.currentCat.count++;
+        catView.render();
+    }
+};
 
-                propertyCopy.count++;
-                catClickCount.textContent = propertyCopy.count;
-            })
-        }
-    })(property));
-})
+var catView = {
+    init: function () {
+        this.catName = document.querySelector('.individual-cat-name');
+        this.catImage = document.querySelector('.individual-cat-images');
+        this.catClickCount = document.querySelector('.individual-cat-count');
+
+        //   on click, increment the current cat's counter
+        this.catImage.addEventListener('click', function () {
+            octopus.incrementCounter();
+        });
+
+        this.render();
+    },
+
+    render: function () {
+        // update the DOM elements with values from the current cat
+        var currentCat = octopus.getCurrentCat();
+        this.catClickCount.textContent = currentCat.count;
+        this.catName.textContent = currentCat.name;
+        this.catImage.src = currentCat.images;
+    }
+};
+
+var catListView = {
+    init: function () {
+        // store the DOM element for easy access later
+        this.catNamesWrapper = document.querySelector('.cat-names');
+
+        this.render();
+    },
+
+    render: function () {
+        // gets the cats we would be rendering from the octopus
+        var properties = octopus.getCats();
+
+        // empty the cat list
+        this.catNamesWrapper.innerHTML = '';
+
+        properties.forEach((property) => {
+
+            // makes a new cat list and set its text
+            var para = document.createElement('p');
+            para.innerHTML = property.name;
+
+
+            para.addEventListener('click', ((propertyCopy) => {
+                return function () {
+                    octopus.setCurrentCat(propertyCopy);
+                    catView.render();
+                };
+            })(property));
+
+            this.catNamesWrapper.appendChild(para);
+        });
+    }
+};
+
+octopus.init();
+
+
+// properties.forEach((property) => {
+//     let para = document.createElement('p');
+//     para.innerHTML = property.name;
+//     catNamesWrapper.appendChild(para);
+
+//     para.addEventListener('click', ((propertyCopy) => {
+//         return () => {
+//             let catName = document.querySelector('.individual-cat-name');
+//             let catImage = document.querySelector('.individual-cat-images');
+//             let catClickCount = document.querySelector('.individual-cat-count');
+
+//             catName.textContent = propertyCopy.name;
+//             catImage.src = propertyCopy.images;
+//             catClickCount.textContent = propertyCopy.count;
+
+//             catImage.addEventListener('click', () => {
+
+//                 propertyCopy.count++;
+//                 catClickCount.textContent = propertyCopy.count;
+//             })
+//         }
+//     })(property));
+// })
 // names.forEach(() => {
 
 //     let catNames = document.querySelector('.cat-names');
